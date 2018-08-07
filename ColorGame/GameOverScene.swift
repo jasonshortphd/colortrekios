76b1bf80ffd34c58f022d2e3c474bca1a7dd5b4f
Jason Short <jasonshortphd@hotmail.com>
2018-08-07 14:53:07 -0700

    Added new sks files for start screen and gameover.  Wired up the start screen to be the actual starting screen, and the button to load the real game.

---
+++ ColorGame/GameOverScene.swift
@@ -0,0 +1,38 @@
+import SpriteKit
+
+class GameOverScene : SKScene
+{
+    var lastScoreLabel:SKLabelNode?
+    var bestScoreLabel:SKLabelNode?
+    
+    var playButton:SKSpriteNode?
+    
+    var backgroundMusic: SKAudioNode!
+    
+    override func didMove(to view: SKView)
+    {
+        lastScoreLabel = self.childNode(withName: "lastScoreLabel") as? SKLabelNode
+        bestScoreLabel = self.childNode(withName: "bestScoreLabel") as? SKLabelNode
+    
+        
+        playButton = self.childNode(withName: "startButton") as? SKSpriteNode
+        
+        if let musicURL = Bundle.main.url(forResource: "MenuHighscoreMusic", withExtension: "mp3") {
+            backgroundMusic = SKAudioNode(url: musicURL)
+            addChild(backgroundMusic)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if let touch = touches.first {
+            let pos = touch.location(in: self)
+            let node = self.atPoint(pos)
+            
+            if node == playButton {
+            
+            }
+        }
+    }
+    
+}
